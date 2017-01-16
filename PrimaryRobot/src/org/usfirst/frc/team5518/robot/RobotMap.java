6f8244c0e180fd85838a34a3fb1a5ab9602e6436
boktah <tbokhar@ncsu.edu>
2017-01-16 16:30:46 -0500

    Added int values for controller axes and buttons.

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
@@ -6,14 +6,40 @@ package org.usfirst.frc.team5518.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+public class RobotMap
+{
+	//WingMan Attack 2 Joystick 
+	//Joystick Axes Assignments
+	public static int JOYSTICK_XAXIS = 0;
+	public static int JOYSTICK_YAXIS = 1;
+	public static int JOYSTICK_ZAXIS = 2;
+	
+	//Joystick Buttons Assignments
+	public static int JOYSTICK_TRIGGERBUTTON = 1;
+	public static int JOYSTICK_BUTTON_02 = 2;
+	public static int JOYSTICK_BUTTON_03 = 3;
+	public static int JOYSTICK_BUTTON_04 = 4;
+	public static int JOYSTICK_BUTTON_05 = 5;
+	public static int JOYSTICK_BUTTON_06 = 6;
+	
+	// Xbox 360/One controller mapping
+	// controller axis
+	public static int XBOX_LSTICKX = 0;
+	public static int XBOX_LSTICKY = 1; 
+	public static int XBOX_RSTICKX = 4;
+	public static int XBOX_RSTICKY = 5;
+	public static int XBOX_LTRIGGER = 2;
+	public static int XBOX_RTRIGGER = 3;
+	
+	// controller buttons
+	public static int XBOX_YBUTTON = 4;
+	public static int XBOX_XBUTTON = 3;
+	public static int XBOX_ABUTTON = 1;
+	public static int XBOX_BBUTTON = 2; 
+	public static int XBOX_RBUMBER = 6;
+	public static int XBOX_LBUMBER = 5;
+	public static int XBOX_LSTICK = 9;
+	public static int XBOX_RSTICK = 10;
+	public static int XBOX_START = 8;
+	public static int XBOX_BACK = 7;
 }
