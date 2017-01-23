448395c6069e70578c1174d67b62939868b9c9d2
boktah <tbokhar@ncsu.edu>
2017-01-23 15:23:15 -0500

    Enabled deadzone and motor safety, commented code

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/DriveTrain.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/DriveTrain.java
@@ -20,13 +20,24 @@ public class DriveTrain extends Subsystem  {
 	public boolean isInverted;
 	
 	public DriveTrain() {
+		//Initialize motors to port numbers from RobotMap
 		frontLeftMotor = new VictorSP(RobotMap.FRONT_LEFT_PORT_NUMBER);
 		frontRightMotor = new VictorSP(RobotMap.FRONT_RIGHT_PORT_NUMBER);
 		backLeftMotor = new VictorSP(RobotMap.BACK_LEFT_PORT_NUMBER);
 		backRightMotor = new VictorSP(RobotMap.BACK_RIGHT_PORT_NUMBER);
+    	//Enable the deadband elimination (the dead zone on the controller)
+		frontLeftMotor.enableDeadbandElimination(true);
+		frontRightMotor.enableDeadbandElimination(true);
+		backLeftMotor.enableDeadbandElimination(true);
+		backRightMotor.enableDeadbandElimination(true);
+		
+		//Initialize driveTrain
+    	driveTrain = new RobotDrive(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor);
+    	//Enable safety on driveTrain and set the time period before safety locks down the motors
+		robotDrive.setSafetyEnabled(true);
+		robotDrive.setExpiration(0.5);
     	
-    	driveTrain = new RobotDrive(leftMotor, rightMotor);
-    	
+		//Init inverted boolean
     	isInverted = false;
 	}
 
