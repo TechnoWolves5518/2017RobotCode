67a4f5440b3451716fb10375b87d30a3af18be79
boktah <tbokhar@ncsu.edu>
2017-01-23 15:14:09 -0500

    Assigned 4 VictorSP instances and added port #s. Made invert bool.

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/DriveTrain.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/DriveTrain.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team5518.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
 
 import org.usfirst.frc.team5518.robot.RobotMap;
 
@@ -15,14 +15,19 @@ import edu.wpi.first.wpilibj.RobotDrive;
 public class DriveTrain extends Subsystem  {
 	
 	RobotDrive driveTrain;
-	Victor leftMotor, rightMotor;
+	VictorSP frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
 	Joystick driveStick;
+	public boolean isInverted;
 	
 	public DriveTrain() {
-		leftMotor = new Victor(RobotMap.LEFT_PORT_NUMBER);
-    	rightMotor = new Victor(RobotMap.RIGHT_PORT_NUMBER);
+		frontLeftMotor = new VictorSP(RobotMap.FRONT_LEFT_PORT_NUMBER);
+		frontRightMotor = new VictorSP(RobotMap.FRONT_RIGHT_PORT_NUMBER);
+		backLeftMotor = new VictorSP(RobotMap.BACK_LEFT_PORT_NUMBER);
+		backRightMotor = new VictorSP(RobotMap.BACK_RIGHT_PORT_NUMBER);
     	
     	driveTrain = new RobotDrive(leftMotor, rightMotor);
+    	
+    	isInverted = false;
 	}
 
 	public void initDefaultCommand() {
