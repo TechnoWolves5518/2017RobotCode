58e6fc3bf3815629018d72d4ae6db84adf1d784e
boktah <tbokhar@ncsu.edu>
2017-01-30 20:40:36 -0500

    Set up base subsystem and command for motor controlling
    Rather than having a separate command and subsystem for each function of
    a motor we will have a base class to which we will pass parameters for
    which motor to control and which way to turn it, as well as which button
    to press to turn that motor.

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/MotorController.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/MotorController.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class FuelShooter extends Subsystem {
+public class MotorController extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
