43dc7b33bc3a14799e5f25d73bc92769f76379a9
boktah <tbokhar@ncsu.edu>
2017-02-13 17:57:04 -0500

    Added SF to DriveTrain for easier testing

---
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/commands/RunMotor.java
@@ -0,0 +1,75 @@
+package org.usfirst.frc.team5518.robot.commands;
+
+import org.usfirst.frc.team5518.robot.OI;
+import org.usfirst.frc.team5518.robot.Robot;
+import org.usfirst.frc.team5518.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RunMotor extends Command {
+	public double LTval;
+	public double RTval;
+	public boolean runWinch, runWinchBack;
+	
+	public RunMotor() {
+	    requires(Robot.motorController);
+		
+		LTval = 0;
+		RTval = 0;
+		runWinch = false;
+		runWinchBack = false;
+	}
+	
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		LTval = OI.sfController.getRawAxis(RobotMap.XBOX_LTRIGGER);
+		RTval = OI.sfController.getRawAxis(RobotMap.XBOX_RTRIGGER);
+		runWinch = OI.getButton(OI.driveController, RobotMap.XBOX_XBTN);
+		runWinchBack = OI.getButton(OI.driveController, RobotMap.XBOX_ABTN);
+		
+		if (LTval > 0.15) {
+			Robot.motorController.runIntakeMotor(true);
+		}
+		else if (LTval < 0.15) {
+			Robot.motorController.runIntakeMotor(false);
+		}
+		
+		if (RTval > 0.15) {
+			Robot.motorController.runShooterMotor(true);
+		}
+		else if (RTval < 0.15) {
+			Robot.motorController.runShooterMotor(false);
+		}
+		
+		if (runWinch) {
+			Robot.motorController.runWinchMotor(true, 1);
+		}
+		else if (runWinchBack) {
+			Robot.motorController.runWinchMotor(true, -1);
+		}
+		if (!runWinch && !runWinchBack) {
+			Robot.motorController.runWinchMotor(false, 0);
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+	    return false;
+	}
+	
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
---
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/subsystems/MotorController.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team5518.robot.subsystems;
+
+import org.usfirst.frc.team5518.robot.RobotMap;
+import org.usfirst.frc.team5518.robot.commands.RunMotor;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class MotorController extends Subsystem {
+	public VictorSP intakeMotor;
+	public VictorSP shooterMotor;
+	public VictorSP winchMotor;
+	
+	public double intakeMotorSpeed;
+	public double shooterMotorSpeed;
+	public double winchMotorSpeed;
+	
+	public MotorController() {
+		intakeMotor = new VictorSP(RobotMap.INTAKE_PORT_NUMBER);
+		shooterMotor = new VictorSP(RobotMap.SHOOTER_PORT_NUMBER);
+		winchMotor = new VictorSP(RobotMap.WINCH_PORT_NUMBER);
+		
+		intakeMotorSpeed = 1;
+		shooterMotorSpeed = 1;
+		winchMotorSpeed = 1;
+	}
+	
+	public void initDefaultCommand() {
+	    // Set the default command for a subsystem here.
+	    setDefaultCommand(new RunMotor());
+	}
+	
+	public void runIntakeMotor(boolean run) {
+		if (run) {
+			intakeMotor.set(intakeMotorSpeed);
+		}
+		else if (!run) {
+			intakeMotor.set(0);
+		}
+	}
+	public void runShooterMotor(boolean run) {
+		if (run) {
+			shooterMotor.set(shooterMotorSpeed);
+		}
+		else if (!run) {
+			shooterMotor.set(0);
+		}
+	}
+	public void runWinchMotor(boolean run, int dir) {
+		if (run) {
+			winchMotor.set(winchMotorSpeed * dir);
+		}
+		else if (!run) {
+			winchMotor.set(0);
+		}
+	}
+}
+
