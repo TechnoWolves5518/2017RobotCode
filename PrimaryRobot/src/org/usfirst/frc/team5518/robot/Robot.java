d034e2a1601b619a4b9aa0db941b95a8d29afad8
Woozl <Wolves@Wolves-PC.attlocal.net>
2017-02-21 18:48:17 -0500

    push

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/Robot.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/Robot.java
@@ -119,7 +119,7 @@ public class Robot extends IterativeRobot {
 	}
 
 	@Override
-	public void teleopInit() {
+	public void teleopInit() { //
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
