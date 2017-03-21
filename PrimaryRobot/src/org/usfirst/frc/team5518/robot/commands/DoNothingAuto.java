5b971f53af47d72ff69d0ae95b1733224a1d2616
boktah <tbokhar@ncsu.edu>
2017-03-21 18:11:58 -0400

    Removed unused import

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/commands/DoNothingAuto.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/commands/DoNothingAuto.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team5518.robot.commands;
 import org.usfirst.frc.team5518.robot.Robot;
 import org.usfirst.frc.team5518.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.command.Command;
-
 /**
  *
  */
