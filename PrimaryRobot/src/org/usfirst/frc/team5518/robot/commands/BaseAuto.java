f31e85f4752fc52f26f68c3b54b5b17031dc620a
boktah <tbokhar@ncsu.edu>
2017-08-01 22:07:32 -0400

    Adjusted backwards smooth auto speed (move slower, go further)

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/commands/BaseAuto.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/commands/BaseAuto.java
@@ -164,7 +164,7 @@ public abstract class BaseAuto extends Command {
 			}
 		}
 		else if (!movingForward && avg != 1000) {
-			speed = avg/35;
+			speed = avg/70;
 			if (speed > 0.5) {
 				speed = 0.5;
 			}
